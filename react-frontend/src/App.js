3441e6a3a3fe6b0d20bcdbeeb69a6a34f191a27e
Ritul Bharati <ritul.pravash@gmail.com>
2024-08-31 19:59:11 +0530

    Login page completed.

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Chatbot from './Chatbot/chatbot';
+//import Chatbot from './Chatbot/chatbot';
+import Login from './usercomponents/login';
 
 function App() {
   return (
     <div className="App">
-      <Chatbot />
+      < Login/>
     </div>
   );
 }
